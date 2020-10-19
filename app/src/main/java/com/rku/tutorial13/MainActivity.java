6aaa7c2b7a930ec19913f6e685421a3540ab52fe
Parth <pgalani193@rku.ac.in>
2020-10-19 18:08:22 +0530

    MainActivity.java Add onClick method call_action, sms_action...

--- app/src/main/java/com/rku/tutorial13/MainActivity.java
+++ app/src/main/java/com/rku/tutorial13/MainActivity.java
@@ -2,13 +2,32 @@ package com.rku.tutorial13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtNumber, edtTextMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNumber = findViewById(R.id.edtNumber);
+        edtTextMessage = findViewById(R.id.edtTextMessage);
+    }
+
+    public void call_action(View view) {
+        String phoneNumber = edtNumber.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel" + phoneNumber));
+        startActivity(intent);
+    }
+
+    public void sms_action(View view) {
+        String phoneNumber = edtNumber.getText().toString();
+        String smsText = edtTextMessage.getText().toString();
     }
 }
